9b40246f0f8494b2d8a47e94e8a9532b9851f683
juann11 <juan.ospinaa@upb.edu.co>
2025-04-28 17:50:19 -0500

    Segundo commit :P

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 import express from 'express'
 
 const app = express();
+
+app.listen(3000, () => {
+console.log('Server is running on port 3000')
+
+});
+
+//Crear un endpoint
+app.get('/', (req, res) => {
+    res.send('Hello World Motherfucker!')
+});
+
+app.post(
+    "/create-user",
+    (req, res) => {
+    
+
+    res.send('hello world')
+
+
+    }
+);
+
+//crea una peticion POST
+
+
